d26ee5c5098bbe589f8b46a7042287b95f172187
MrDrummer25 <mrdrummer.programming@hotmail.com>
2018-11-29 20:31:52 +0000

    Readable time
    Fixed undefined clients connected issue

--- web/client.js
+++ web/client.js
@@ -15,9 +15,9 @@ socket.onmessage = function (event) {
   let response = (JSON.parse(event.data))
   if (response.data) {
     data = response.data.split(",")
-    document.getElementById("data").innerHTML = "Unix: " + data[0] + " Soil: " + data[1] + " Temperature: " + data[2] + " Light: " + data[3] + " Humidity: " + data[4]
+    document.getElementById("data").innerHTML = "Time: " + unixToTime(data[0]) + " Soil: " + data[1] + " Temperature: " + data[2] + " Light: " + data[3] + " Humidity: " + data[4]
   } else if (response.clients) {
-    document.getElementById("clients").innerHTML = "Clients connected: " + data.clients
+    document.getElementById("clients").innerHTML = "Clients connected: " + response.clients
   } else {
     console.error("Received data I do not recognise!", event)
   }
@@ -31,3 +31,11 @@ window.addEventListener('beforeunload', function() {
   console.log("websocket closed")
   socket.close()
 })
+
+function unixToTime(t) {
+  var dt = new Date(t*1000);
+  var hr = dt.getHours();
+  var m = "0" + dt.getMinutes();
+  var s = "0" + dt.getSeconds();
+  return hr+ ':' + m.substr(-2) + ':' + s.substr(-2);  
+}
