9d5294b1ad3f27a7e408aa7c98f8053d7bbe5a07
MrDrummer25 <mrdrummer.programming@hotmail.com>
2018-11-30 22:01:20 +0000

    "key" was being interpreted as a key instead of its value...

--- web/client.js
+++ web/client.js
@@ -50,7 +50,9 @@ async function populateData() {
 function updateValues (data) {
   console.log("updateValue data", data)
   let key = (Object.keys(data))[0]
-  document.getElementById("data").innerHTML = "Time: " + unixToTime(key) + " Light: " + data.key.light + " Temperature: " + data.key.temp + " Humidity: " + data.key.humidity + " Soil: " + data.key.soil
+  console.log("key", key)
+  console.log("data.key", data[key])
+  document.getElementById("data").innerHTML = "Time: " + unixToTime(key) + " Light: " + data[key].light + " Temperature: " + data[key].temp + " Humidity: " + data[key].humidity + " Soil: " + data[key].soil
 }
 
 populateData()
